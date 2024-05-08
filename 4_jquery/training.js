df3d5bb7cdd03855e96677b0623362c21282229d
dexter <09010262621230eq@gmail.com>
2024-05-08 21:34:12 +0800

    pushed this super late because of integration testing

---
+++ 4_jquery/training.js
@@ -0,0 +1,32 @@
+let allAuthors = [];
+const books = [
+  {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    pages: 180,
+    year: 1925,
+    isbn: "9780743273565"
+  },
+  {
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    pages: 281,
+    year: 1960,
+    isbn: "9780061120084"
+  },
+  {
+    title: "1984",
+    author: "George Orwell",
+    pages: 328,
+    year: 1949,
+    isbn: "9780451524935"
+  }
+];
+// Write a function here
+function getInventory(allAuthors) {
+  for(let i=0; i<books.length; i++){
+    allAuthors.push;
+    return allAuthors;
+  }
+}
+console.log(getInventory(books));
